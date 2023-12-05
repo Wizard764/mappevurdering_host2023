9f74e3a909bca445d25add03554e63576a327b1a
Jonas Hazeland Baugerud <jonas.baugerud@gmail.com>
2023-12-05 12:39:14 +0100

    Moved test code to start() and initialization to init(). Refactored test code. Added method to print train departures to console.

--- src/main/java/edu/ntnu/stud/Wizard764/UserInterface.java
+++ src/main/java/edu/ntnu/stud/Wizard764/UserInterface.java
@@ -1,29 +1,47 @@
 package edu.ntnu.stud.Wizard764;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
 
+/**
+ * Class to handle the user interface as well as high level program flow.
+ */
 public class UserInterface {
-  private ArrayList<TrainDeparture> trainDepartures;
+  private TrainDepartureRegistry tdr;
 
   UserInterface() {
-    trainDepartures = new ArrayList<>();
+
   }
 
   public void init() {
-    LocalTime[] departureTimes = {LocalTime.of(9, 30), LocalTime.of(10, 30), LocalTime.parse("00:05")};
-    String[] lines = {"TestLine", "R60", "F4"};
-    String[] trainNumbers = {"1771", "123", "ABC123"};
-    String[] destinations = {"TestDestination", "Trondheim", "This is a loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong destination name"};
-    LocalTime[] delays = {LocalTime.parse("01:08"), LocalTime.of(0, 15)};
-    short[] tracks = {4, 1};
-    String comment = "Few tickets left";
-    trainDepartures.add(new TrainDeparture(departureTimes[0], lines[0], trainNumbers[0], destinations[0], delays[0], tracks[0]));
-    trainDepartures.add(new TrainDeparture(departureTimes[1], lines[1], trainNumbers[1], destinations[1]));
-    trainDepartures.add(new TrainDeparture(departureTimes[2], lines[2], trainNumbers[2], destinations[2], delays[1], tracks[1], comment));
+    tdr = new TrainDepartureRegistry();
   }
 
+  /**
+   * "Main program method"
+   * Runs the user interface and manages the train departure registry based on user input.
+   */
   public void start() {
+    LocalTime[] departureTimes = {LocalTime.of(9, 30), LocalTime.of(10, 30), LocalTime.of(0, 5)};
+    String[] lines = {"TestLine", "R60", "F4"};
+    String[] trainNumbers = {"1771", "123", "ABC123"};
+    String[] destinations = {"TestDestination", "Trondheim",
+        "This is a loooooooooooooooooooooooooooooooooooooooooooooooooooooooong destination name"
+    };
+    LocalTime[] delays = {LocalTime.of(1, 8), LocalTime.of(0, 15), LocalTime.of(0, 0)};
+    short[] tracks = {4, 1, -1};
+    for (int i = 0; i < departureTimes.length; i++) {
+      tdr.addDeparture(new TrainDeparture(departureTimes[i], lines[i],
+              trainNumbers[i], destinations[i], delays[i], tracks[i]));
+    }
+    printInformationBoard();
+  }
 
+  /**
+   * Prints a visual representation of all departures stored in the registry to the console.
+   * Departures are sorted by departure time, not including delay.
+   */
+  public void printInformationBoard() {
+    tdr.sortDeparturesByTime();
+    System.out.println(tdr);
   }
 }
